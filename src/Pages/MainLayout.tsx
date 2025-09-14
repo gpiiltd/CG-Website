1de8ae2cd6affc5f447c3a7da92fe649d97e8924
Chioma Theresa Nwabugwu <chiomanwabugwu11@yahoo.com>
2025-09-14 22:07:16 +0100

    Router | Header (Footer in progress)

---
+++ src/Pages/MainLayout.tsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import Header from '../Components/Header';
+
+const MainLayout = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return (
+    <div className="min-h-screen flex flex-col w-full bg-white">
+      <Header />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default MainLayout;
