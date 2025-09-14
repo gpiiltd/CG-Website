bc57ecf2e1aedc28001532adcc66a02352c91850
maximdave <enoragbondavid35@gmail.com>
2025-09-14 18:40:44 +0100

    CGW-17 | add list of directors

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Icon from './assets/SvgImagesAndIcons';
+// import Icon from './assets/SvgImagesAndIcons';
+import FacesOfCG from './Pages/FacesOfCG';
 function App() {
   return (
     <>
-      <div>
+      {/* <div>
         <Icon type="cgLogo" className="w-10 h-10" />
         <h1>Century Website </h1>
+      </div> */}
+
+      <div>
+        <FacesOfCG />
       </div>
     </>
   );
--- src/assets/SvgImagesAndIcons.tsx
+++ src/assets/SvgImagesAndIcons.tsx
@@ -4,7 +4,12 @@ import cgLogo from './svgImages/cgLogo.svg';
 import menPipe from './svgImages/menPipe.svg';
 import lightBulb from './svgImages/lightBulb.svg';
 import kenEtete from './svgImages/kenEtete.svg';
-import osaretin from './svgImages/osaretin.svg';
+import osaretinAmadasu from './svgImages/osaretinAmadasu.svg';
+import maclarenKosuwei from './svgImages/maclarenKosuwei.svg';
+import osasUwaifo from './svgImages/osasUwaifo.svg';
+import edwardYinkere from './svgImages/edwardYinkere.svg';
+import teikariyeEtete from './svgImages/teikariyeEtete.svg';
+import preyeAngaye from './svgImages/preyeAngaye.svg';
 
 const Icon: React.FC<Svgprops> = ({ type, className, click }) => {
   const renderIcon = () => {
@@ -17,8 +22,18 @@ const Icon: React.FC<Svgprops> = ({ type, className, click }) => {
         return <img src={lightBulb} alt="lightBulb" className={className} />;
       case 'kenEtete':
         return <img src={kenEtete} alt="kenEtete" className={className} />;
-      case 'osaretin':
-        return <img src={osaretin} alt="osaretin" className={className} />;
+      case 'osaretinAmadasu':
+        return <img src={osaretinAmadasu} alt="osaretinAmadasu" className={className} />;
+      case 'maclarenKosuwei':
+        return <img src={maclarenKosuwei} alt="maclarenKosuwei" className={className} />;
+      case 'osasUwaifo':
+        return <img src={osasUwaifo} alt="osasUwaifo" className={className} />;
+      case 'edwardYinkere':
+        return <img src={edwardYinkere} alt="edwardYinkere" className={className} />;
+      case 'teikariyeEtete':
+        return <img src={teikariyeEtete} alt="teikariyeEtete" className={className} />;
+      case 'preyeAngaye':
+        return <img src={preyeAngaye} alt="preyeAngaye" className={className} />;
       default:
         return null;
     }
