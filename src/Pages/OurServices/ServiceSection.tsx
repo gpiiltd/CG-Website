936072b266fd514161de5548862e889953c288e7
maximdave <enoragbondavid35@gmail.com>
2025-09-25 22:52:13 +0100

    CGW-47 | update services images

--- src/Pages/OurServices/ServiceSection.tsx
+++ src/Pages/OurServices/ServiceSection.tsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { ButtonComponent } from '../../Components/ButtonComponent';
 import type { ServiceSectionProps } from '../../Components/Interfaces';
+import LazyImage from '../../Components/LazyImage';
 
 const ServiceSection: React.FC<ServiceSectionProps> = ({
   bgColor = '#F5F1FE',
@@ -21,15 +22,26 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
 
     <div className="max-w-[95%] mx-auto px-6 py-16 grid lg:grid-cols-2 gap-12 ">
       {/* Left side images */}
-      <div className="grid grid-cols-2 gap-4">
-        {images.map((img, idx) => (
-          <img
-            key={idx}
-            src={img}
-            alt={`Service ${idx + 1}`}
-            className={`rounded-lg object-cover w-full ${idx === 1 ? 'row-span-2' : 'h-48'}`}
+      <div className="flex gap-4 h-max justify-center">
+        <div className="flex flex-col gap-4 h-full">
+          <LazyImage
+            src={images[0]}
+            alt="serviceImage"
+            className="rounded-2xl object-cover w-full"
           />
-        ))}
+          <LazyImage
+            src={images[1]}
+            alt="serviceImage"
+            className="rounded-2xl object-cover w-full"
+          />
+        </div>
+        <div>
+          <LazyImage
+            src={images[2]}
+            alt="serviceImage"
+            className="rounded-2xl object-cover w-full h-full"
+          />
+        </div>
       </div>
 
       {/* Right side text */}
@@ -38,12 +50,12 @@ const ServiceSection: React.FC<ServiceSectionProps> = ({
           {companyTitle}
         </h2>
         {companyDescription?.split('\n\n').map((para, idx) => (
-          <p key={idx} className="text-gray-700 mb-4 font-medium">
+          <p key={idx} className="text-gray-700 mb-8 font-medium">
             {para.trim()}
           </p>
         ))}
         <Link to={contactLink}>
-          <ButtonComponent text="Contact us" bg_color="#ED6C30" />
+          <ButtonComponent text="Contact us " bg_color="#ED6C30" />
         </Link>
       </div>
     </div>
--- src/Pages/OurServices/servicesDatalist.tsx
+++ src/Pages/OurServices/servicesDatalist.tsx
@@ -1,40 +1,42 @@
-import shore from '../../assets/svgImages/shore.svg';
 import manshore from '../../assets/svgImages/manshore.svg';
 import womanworker from '../../assets/svgImages/womanworker.svg';
 import gasplant from '../../assets/svgImages/gasplant.svg';
-import manworker from '../../assets/svgImages/manworker.svg';
+import atalaEP from '../../assets/Atala-E&P.png';
+import tnFPSO from '../../assets/TN FPSO- Deployment - O&M.jpg';
+import LNGVessel from '../../assets/LNG Vessel.jpg';
+import CPTLOps from '../../assets/CPTL Ops.jpeg';
 
 export const overlayData = [
   {
     id: 1,
     title: 'Deployment, Operation and maintenance of Offshore infrastructure',
     description:
-      'A detailed wireframe kit made of comprehensive components to get you started on your next website or landing page project.',
-    image: manshore,
+      'Century Group provides integrated lifecycle services for the deployment, operations, and maintenance of critical offshore energy infrastructure.',
+    image: atalaEP,
     bgColor: '#EDFFFF',
   },
   {
     id: 2,
     title: 'Exploration and production',
     description:
-      'A detailed wireframe kit made of comprehensive components to get you started on your next website or landing page project.',
-    image: womanworker,
+      'Century Group’s Exploration & Production of Hydrocarbons service is built on deep industry expertise and a commitment to unlocking energy resources responsibly.',
+    image: tnFPSO,
     bgColor: '#F5F1FE',
   },
   {
     id: 3,
     title: 'Ports Terminal and Management Services',
     description:
-      'A detailed wireframe kit made of comprehensive components to get you started on your next website or landing page project.',
-    image: manworker,
+      'Century Group delivers integrated Ports & Terminal Management services, ensuring the safe, reliable, and efficient handling of hydrocarbons and cargo at strategic maritime hub "Port of Kidney Island".',
+    image: CPTLOps,
     bgColor: '#F6E4F0',
   },
   {
     id: 4,
     title: 'Natural Gas Transmission and Distribution',
     description:
-      'A detailed wireframe kit made of comprehensive components to get you started on your next website or landing page project.',
-    image: gasplant,
+      'Century Group’s Natural Gas Transmission & Distribution service provides reliable midstream solutions that connect resources to markets.',
+    image: LNGVessel,
     bgColor: '#FEF3EF',
   },
 ];
@@ -44,8 +46,8 @@ export const serviceSectionsData = [
     bgColor: '#EDFFFF',
     title: 'Deployment, Operation and maintenance of Offshore infrastructure',
     subtitle:
-      'From offshore infrastructure deployment, to energy logistics, our integrated services are designed too optimize operation, minimize risk and maximize, value for stakeholders.',
-    images: [shore, shore, shore],
+      'Century Group provides integrated lifecycle services for the deployment, operations, and maintenance of critical offshore energy infrastructure.',
+    images: [atalaEP, atalaEP, atalaEP],
     companyTitle: 'Century Energy Services Limited (CESL)',
     companyDescription: `Century Group provides integrated lifecycle services for the deployment, operations, and maintenance of critical offshore energy infrastructure. Our comprehensive approach ensures assets are commissioned safely and efficiently, transitioning seamlessly into stable production phases. We specialize in managing the full spectrum of offshore facilities, from fixed platforms to floating systems, with a relentless focus on operational excellence, asset integrity, and minimizing lifecycle costs from day one.\n\n
   Execution is underpinned by robust management systems and a deep commitment to Health, Safety, and Environment (HSE). We employ standardized processes and advanced digital solutions to optimize production uptime, plan and execute complex maintenance campaigns, and manage logistics in challenging offshore environments. This disciplined methodology ensures predictable performance, safeguards our personnel and assets, and protects the surrounding environment throughout the asset's operational life.\n\n
@@ -57,8 +59,8 @@ export const serviceSectionsData = [
     bgColor: '#F5F1FE',
     title: 'Exploration and production',
     subtitle:
-      'From offshore infrastructure deployment, to energy logistics, our integrated services are designed too optimize operation, minimize risk and maximize, value for stakeholders.',
-    images: [shore, shore, shore],
+      'Century Group’s Exploration & Production of Hydrocarbons service is built on deep industry expertise and a commitment to unlocking energy resources responsibly.',
+    images: [tnFPSO, tnFPSO, tnFPSO],
     companyTitle: 'Century Exploration & Production Limited (CEPL)',
     companyDescription: `Century Group’s Exploration & Production of Hydrocarbons service is built on deep industry expertise and a commitment to unlocking energy resources responsibly. Through reliable partnerships and access to novel technologies in advanced seismic interpretation, subsurface evaluation, and optimized drilling programs, we deliver end-to-end solutions that ensure efficient access to oil and gas reserves. Our approach integrates cutting-edge technology with disciplined project management, enabling us to safely bring hydrocarbons from discovery through development.\n\n
 We provide a full spectrum of upstream capabilities, from exploration and appraisal to field strong local insight, the Century Group dedicated company – Century Exploration & Production Limited (CEPL) partners with clients and stakeholders to maximize reservoir value, enhance recovery, and extend asset life. Our teams deploy fit-for-purpose methodologies that balance cost efficiency with environmental stewardship, ensuring sustainable energy development.\n\n
@@ -71,8 +73,8 @@ At CEPL, exploration and production are more than technical processes; they repr
     bgColor: '#F6E4F0',
     title: 'Ports Terminal and Management Services',
     subtitle:
-      'From offshore infrastructure deployment, to energy logistics, our integrated services are designed too optimize operation, minimize risk and maximize, value for stakeholders.',
-    images: [shore, shore, shore],
+      'Century Group delivers integrated Ports & Terminal Management services, ensuring the safe, reliable, and efficient handling of hydrocarbons and cargo at strategic maritime hub "Port of Kidney Island".',
+    images: [CPTLOps, CPTLOps, CPTLOps],
     companyTitle: 'Century Ports & Terminal Limited (CPTL)',
     companyDescription: `Century Group delivers integrated Ports & Terminal Management services, ensuring the safe, reliable, and efficient handling of hydrocarbons and cargo at strategic maritime hub "Port of Kidney Island". Our end-to-end management encompasses the entire logistics chain, from vessel berthing and cargo transfer to storage and distribution. We focus on maximizing asset utilization and throughput while upholding the highest standards of operational integrity, thereby ensuring these critical infrastructures function as reliable links in the global energy supply chain.\n\n
 
@@ -86,8 +88,8 @@ Our special purpose subsidiary- Century Ports & Terminals Limited is committed t
     bgColor: '#FEF3EF',
     title: 'Natural Gas Transmission and Distribution',
     subtitle:
-      'From offshore infrastructure deployment, to energy logistics, our integrated services are designed too optimize operation, minimize risk and maximize, value for stakeholders.',
-    images: [shore, shore, shore],
+      'Century Group’s Natural Gas Transmission & Distribution service provides reliable midstream solutions that connect resources to markets.',
+    images: [LNGVessel, LNGVessel, LNGVessel],
     companyTitle: 'Gas Plus Synergy (GPS)',
     companyDescription: `Century Group’s Natural Gas Transmission & Distribution service provides reliable midstream solutions that connect resources to markets. We specialize in the safe and efficient handling of Liquefied Natural Gas (LNG) and Liquefied Petroleum Gas (LPG), leveraging advanced infrastructure to enable seamless transportation from production sites to end users. Our expertise ensures that natural gas is delivered where it is needed most, supporting domestic consumption. \n\n
      Through a network of virtual pipelines, storage, and processing facilities, we deliver fit-for-purpose solutions that optimize gas transmission and distribution. Our LNG and LPG operations are designed for flexibility—supporting large-scale power generation, industrial feedstock, and household energy needs. By integrating global best practices in gas handling with regional insights, we ensure operational efficiency, cost-effectiveness, and minimal environmental impact. \n\n
