afed686bbca14f7c8b9ee4d090c371aa5be9fd28
kitoga <jken04680@gmail.com>
2025-10-28 14:48:47 +0100

    complete sanity merge with code

--- src/Components/discovery/DiscoveryTimeline.tsx
+++ src/Components/discovery/DiscoveryTimeline.tsx
@@ -5,53 +5,6 @@ import Icon from '../../assets/SvgImagesAndIcons';
 import { Typography } from '../Typography';
 import LazyImage from '../LazyImage';
 
-const years = [
-  {
-    year: 2003,
-    milestones: [
-      'Supported Dresser Rand in the operation and maintenance of the Eku flowstation, OBNAGG, SPDC PEB facilities, SOKU booster station, Nun-River flowstation, Izonbe flowstation, Osa platform. Entered a JV with Bumi Armada to provide EPCI, O&M of FPSO, FSO, E',
-      'Mobilization, Installation, Operation and maintenance of the FPSO Armada Perkassa for Okoro-Setu field development on behalf of Afren/Amni',
-      'Integration of a robust and effective security network across the Niger- Delta;',
-      'Strategic intervention that saved Chevron/Conoil JV a revenue loss of over $985m accrued from shut-in of the Pennington terminal.',
-      'EPCI, O & M of Armada Perdana FPSO of the FPSO Armada Perdana for Oyo field development on behalf of CAM.',
-      'Acquisition of the first CESL wholly owned offshore support vessel- Akassa Supporter line handling tug',
-      'Engineering services and project management support for the repairs of SPDC-owned offloading buoy in Bonny.',
-    ],
-  },
-  {
-    year: 2010,
-    milestones: [
-      'Entered a partnership with the Niger-Delta Amnesty program to train and engage the active manpower available from the Amnesty program.',
-      'Secured a 15 years License to Operate the Kidney Island Logistics base on behalf of SPDC',
-      'Secured an Engineering, Technical Manpower and Logistics support services contract for Ebok MOPU & FSO for Ebok field development on behalf of Afren and Oriental.',
-      'Technical Services, O & M of the FPSO Mystras (OML 119) on a 3+1 contract term on behalf of NPDC.',
-      'Technical Services, O & M of the Okpoho production platform (OML 119) on behalf of NPDC',
-      'Technical Services, O & M of OML 42 facilities on behalf of NPDC (the biggest asset divested by SPDC in 2011- 7 flowstations -production operations from BATAN flowstation',
-      'Expansion of our in-country vessels fleet to offer marine support services for our diverse clients',
-      'Acquisition of the busy bee oil tanker (renamed Principe Trader)',
-      'Initiated discussion with Bayelsa Oil Company and Bayelsa state government for a farm-in agreement for development of the state-owned marginal field (Atala field)',
-      'Signed the Joint Operating Agreement (JOA) for 29% economic interest and 80% working interest in ATALA field development',
-    ],
-  },
-  {
-    year: 2019,
-    milestones: [
-      'Partnered with PTT Public company Limited (Thailand National Oil Company) to lift and export 30,000bbls/day of crude on behalf of NNPC',
-      'Secured a Technical Services, O & M contract for OML 40 facilities on behalf of NPDC (the only asset not producing during SPDC divestment',
-      'Secured a term contract from NNPC to lift Oso Condensates and Natural Gas Liquids (NGLs)',
-      'Secured a one-year term contract from NPDC to lift and trade 30,000bbls/day of crude',
-      'Successful re-entry of Atala-1 well and commencement of production from Atala field',
-      'Commissioned and commencement of production from Odidi flowstation located in OML 42',
-      'Aggressively drove diverse Gas Utilization & commercialization solutions, including the “GAS-TO-POWER INITIATIVE”',
-      'Secured a Gas Purchase Order from the GACN to acquire gas from SPDC for power generation projects.',
-      'Secured a domestic gas distribution deal with NLNG for 500,000 metric tons of liquefied natural gas per annum.',
-      'Initiated and completed the purchase of 3 FPSOs – FPSO TAMARA TOKONI, NANAYE & ELMINA.',
-      'Designed and executed an alternative Crude Export Solution that made the resumption of operations at the Forcados Export Terminal on behalf of SHELL.',
-      'Facilitated the storage and export of the maiden cargo of the Obodo crude blend on behalf of ConOil.',
-    ],
-  },
-];
-
 interface TimelineYear {
   year: number;
   milestones: string[];
